459c2d56619a640c7d4bf18951fccb37afeb7f5d
215023902 <215023902@ISTR19-550>
2019-04-10 11:43:41 +0200

    aaa

--- WindowsApplication1/WindowsApplication1/Form6.Designer.vb
+++ WindowsApplication1/WindowsApplication1/Form6.Designer.vb
@@ -22,8 +22,254 @@ Partial Class Form6
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form6))
+        Me.Group29DataSet = New WindowsApplication1.group29DataSet()
+        Me.OrderLineBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.OrderLineTableAdapter = New WindowsApplication1.group29DataSetTableAdapters.OrderLineTableAdapter()
+        Me.TableAdapterManager = New WindowsApplication1.group29DataSetTableAdapters.TableAdapterManager()
+        Me.OrderLineBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
+        Me.OrderLineBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
+        Me.OrderLineDataGridView = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.Group29DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.OrderLineBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.OrderLineBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.OrderLineBindingNavigator.SuspendLayout()
+        CType(Me.OrderLineDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Group29DataSet
+        '
+        Me.Group29DataSet.DataSetName = "group29DataSet"
+        Me.Group29DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'OrderLineBindingSource
+        '
+        Me.OrderLineBindingSource.DataMember = "OrderLine"
+        Me.OrderLineBindingSource.DataSource = Me.Group29DataSet
+        '
+        'OrderLineTableAdapter
+        '
+        Me.OrderLineTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.CustomerTableAdapter = Nothing
+        Me.TableAdapterManager.IngredientTableAdapter = Nothing
+        Me.TableAdapterManager.MenuItemTableAdapter = Nothing
+        Me.TableAdapterManager.OrderLineTableAdapter = Me.OrderLineTableAdapter
+        Me.TableAdapterManager.OrderPaymentTableAdapter = Nothing
+        Me.TableAdapterManager.OrderTBLTableAdapter = Nothing
+        Me.TableAdapterManager.RecipeTableAdapter = Nothing
+        Me.TableAdapterManager.ReservationTableAdapter = Nothing
+        Me.TableAdapterManager.ReservationTableTableAdapter = Nothing
+        Me.TableAdapterManager.TableTBLTableAdapter = Nothing
+        Me.TableAdapterManager.TimeSlotTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = WindowsApplication1.group29DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        Me.TableAdapterManager.WaiterTableAdapter = Nothing
+        '
+        'OrderLineBindingNavigator
+        '
+        Me.OrderLineBindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
+        Me.OrderLineBindingNavigator.BindingSource = Me.OrderLineBindingSource
+        Me.OrderLineBindingNavigator.CountItem = Me.BindingNavigatorCountItem
+        Me.OrderLineBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.OrderLineBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.OrderLineBindingNavigatorSaveItem})
+        Me.OrderLineBindingNavigator.Location = New System.Drawing.Point(0, 0)
+        Me.OrderLineBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.OrderLineBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.OrderLineBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.OrderLineBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.OrderLineBindingNavigator.Name = "OrderLineBindingNavigator"
+        Me.OrderLineBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
+        Me.OrderLineBindingNavigator.Size = New System.Drawing.Size(532, 25)
+        Me.OrderLineBindingNavigator.TabIndex = 0
+        Me.OrderLineBindingNavigator.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 25)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 23)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(35, 15)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 20)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(23, 20)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 20)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
+        'OrderLineBindingNavigatorSaveItem
+        '
+        Me.OrderLineBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.OrderLineBindingNavigatorSaveItem.Image = CType(resources.GetObject("OrderLineBindingNavigatorSaveItem.Image"), System.Drawing.Image)
+        Me.OrderLineBindingNavigatorSaveItem.Name = "OrderLineBindingNavigatorSaveItem"
+        Me.OrderLineBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 23)
+        Me.OrderLineBindingNavigatorSaveItem.Text = "Save Data"
+        '
+        'OrderLineDataGridView
+        '
+        Me.OrderLineDataGridView.AutoGenerateColumns = False
+        Me.OrderLineDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.OrderLineDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4})
+        Me.OrderLineDataGridView.DataSource = Me.OrderLineBindingSource
+        Me.OrderLineDataGridView.Location = New System.Drawing.Point(115, 65)
+        Me.OrderLineDataGridView.Name = "OrderLineDataGridView"
+        Me.OrderLineDataGridView.Size = New System.Drawing.Size(300, 220)
+        Me.OrderLineDataGridView.TabIndex = 1
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "Invoice_ID"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Invoice_ID"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "Menu_Item_ID"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Menu_Item_ID"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "Order_Line_Quantity"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Order_Line_Quantity"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "Order_Line_Unit_Price"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Order_Line_Unit_Price"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        '
+        'Form6
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(532, 358)
+        Me.Controls.Add(Me.OrderLineDataGridView)
+        Me.Controls.Add(Me.OrderLineBindingNavigator)
+        Me.Name = "Form6"
         Me.Text = "Form6"
+        CType(Me.Group29DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.OrderLineBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.OrderLineBindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.OrderLineBindingNavigator.ResumeLayout(False)
+        Me.OrderLineBindingNavigator.PerformLayout()
+        CType(Me.OrderLineDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Group29DataSet As group29DataSet
+    Friend WithEvents OrderLineBindingSource As BindingSource
+    Friend WithEvents OrderLineTableAdapter As group29DataSetTableAdapters.OrderLineTableAdapter
+    Friend WithEvents TableAdapterManager As group29DataSetTableAdapters.TableAdapterManager
+    Friend WithEvents OrderLineBindingNavigator As BindingNavigator
+    Friend WithEvents BindingNavigatorAddNewItem As ToolStripButton
+    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
+    Friend WithEvents BindingNavigatorDeleteItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
+    Friend WithEvents OrderLineBindingNavigatorSaveItem As ToolStripButton
+    Friend WithEvents OrderLineDataGridView As DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
 End Class
--- WindowsApplication1/WindowsApplication1/Form6.vb
+++ WindowsApplication1/WindowsApplication1/Form6.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form6
+    Private Sub OrderLineBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles OrderLineBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.OrderLineBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.Group29DataSet)
 
+    End Sub
+
+    Private Sub Form6_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Group29DataSet.OrderLine' table. You can move, or remove it, as needed.
+        Me.OrderLineTableAdapter.Fill(Me.Group29DataSet.OrderLine)
+
+    End Sub
 End Class
